4df191b22651485e6924e20cadc28e24a28915ea
mdazadhossain95 <mdazadhossain95@gmail.com>
2024-02-27 03:19:38 +0800

    adding login with supabase

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,71 @@ import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:supabase_test/views/home_screen.dart';
 
+import 'views/magic_link.dart';
+import 'views/phone_sign_in.dart';
+import 'views/phone_sign_up.dart';
+import 'views/sign_in.dart';
+import 'views/update_password.dart';
+import 'views/verify_phone.dart';
+
 void main() async {
   await Supabase.initialize(
     url: 'https://lygzgxjqfwyafpbbxmwn.supabase.co',
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5Z3pneGpxZnd5YWZwYmJ4bXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU5Njg0NDksImV4cCI6MjAwMTU0NDQ0OX0.Jj6pJy4r6j3GY7bUBNJ79JqHnVMgrb-TdSx24hv2QZ8',
+    authOptions: const FlutterAuthClientOptions(
+      authFlowType: AuthFlowType.pkce,
+    ),
+    realtimeClientOptions: const RealtimeClientOptions(
+      logLevel: RealtimeLogLevel.info,
+    ),
+    storageOptions: const StorageClientOptions(
+      retryAttempts: 10,
+    ),
   );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(),
         ),
-        home: const HomeScreen());
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SignUp(),
+        '/magic_link': (context) => const MagicLink(),
+        '/update_password': (context) => const UpdatePassword(),
+        '/phone_sign_in': (context) => const PhoneSignIn(),
+        '/phone_sign_up': (context) => const PhoneSignUp(),
+        '/verify_phone': (context) => const VerifyPhone(),
+        '/home': (context) => const HomeScreen(),
+      },
+      onUnknownRoute: (RouteSettings settings) {
+        return MaterialPageRoute<void>(
+          settings: settings,
+          builder: (BuildContext context) => const Scaffold(
+            body: Center(
+              child: Text(
+                'Not Found',
+                style: TextStyle(
+                  fontSize: 42,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
